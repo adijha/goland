d840cdafa389019287b6c615ef462495ee0372ea
Aditya Kumar Jha <mail@adijha.com>
2021-06-30 23:03:19 +0545

    api-tested

--- chat/go.mod
+++ chat/go.mod
@@ -2,4 +2,7 @@ module chat
 
 go 1.16
 
-require github.com/gofiber/fiber/v2 v2.13.0 // indirect
+require (
+	github.com/gofiber/fiber/v2 v2.13.0 // indirect
+	github.com/pusher/pusher-http-go v4.0.1+incompatible // indirect
+)
--- chat/main.go
+++ chat/main.go
@@ -3,15 +3,33 @@ package main
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/pusher/pusher-http-go"
 )
 
 func main() {
 	app := fiber.New()
 	app.Use(cors.New())
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World 👋!")
+	pusherClient := pusher.Client{
+		AppID:   "**",
+		Key:     "**",
+		Secret:  "**",
+		Cluster: "ap2",
+		Secure:  true,
+	}
+
+	app.Post("/api/messages", func(c *fiber.Ctx) error {
+		var data map[string]string
+
+		if err := c.BodyParser(&data); err != nil {
+			return err
+		}
+
+		pusherClient.Trigger("chat", "message", data)
+
+		return c.JSON([]string{})
 	})
+
 
 	app.Listen(":8000")
 }
