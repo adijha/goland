c8012bb10cc6856fc73279863c500448d42daf40
Aditya Kumar Jha <mail@adijha.com>
2021-07-04 02:24:03 +0545

    regular-push

---
+++ jwt/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	_, err :=gorm.Open(mysql.Open("root:787898sql@/jwt"), &gorm.Config{})
+	if err != nil{
+		panic("could not open database connection")
+	}
+
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World 👋!")
+	})
+
+	app.Listen(":8000")
+}
